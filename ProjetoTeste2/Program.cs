ec3f7eafe4bfe4e2be8679454af2d5f5d4b6f246
Christian Muniz <christian_muniz1@hotmail.com>
2022-06-30 23:19:18 -0300

    first commits

---
+++ ProjetoTeste2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.thereading.Teskes;
+
+namespace ProjetoTesteFST2
+{
+
+    public class Operacoes
+    {
+        internal static object Somar(double pNum, object sNum)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ ProjetoTeste2/Unitest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjetoTesteFST2;
+using ProjetoTesteFSTI;
+
+namespace TestMSTest
+{
+	[TesteClass]
+	{
+	public class UnitTest2
+	{
+		[TestMethod]
+		public void SomarDoisNumeros()
+		{
+			//Abrabge - Preparação
+			double pNum = 1;
+			double SNum = 1;
+			double rNum = 2;
+
+			//Act - Ação
+			var resultado = Operacoes.Somar(pNum, sNum);
+
+            //Assert - Verificação
+            Assert.AreEqual(sNum, resultado);
+		}
+
+		internal class TestMethodAttribute : Attribute
+		{
+		}
+	}
